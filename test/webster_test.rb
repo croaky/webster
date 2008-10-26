1aefc822deaec0f84e6a197b5bc38608a44afb37
Dan Croak <dcroak@thoughtbot.com>
2008-10-26 16:22:56 -0400

    bump the gemspec

--- test/webster_test.rb
+++ test/webster_test.rb
@@ -7,6 +7,26 @@ require File.join(File.dirname(__FILE__), "..", "lib", "webster")
 
 class WebsterTest < Test::Unit::TestCase
 
+  context "#dictionary" do
+    setup do
+      @webster = Webster.new
+      @dictionary = @webster.dictionary
+      @blacklist = %w(bastaard bastard bastardly bastardy bedamn bitch damnable damnably damnation
+        damnatory damned damner damnify damnii damning damningly damnonii damnous damnously 
+        shita shitepoke shother shittah shittim undamned undamning undamn
+        assman whore whoredom whorelike whoreship whoreson whorish whorishly
+        hemipenis penis penistone vagina vaginal vaginant vaginate vaginated
+        vagina vaginal vaginant vaginate vaginated dildo dillweed dingus dinkum
+        swordick pussy anusim anusvara)
+    end
+
+    should "not contain offensive words or offensive-sounding words" do
+      @blacklist.each do |word|
+        assert !@dictionary.include?(word), "The blacklist word, #{word}, is in the dictionary."
+      end
+    end
+  end
+
   context "#random" do
     setup do
       @webster = Webster.new
